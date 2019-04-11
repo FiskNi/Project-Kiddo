9f353220a0a1001554a300a4bc01d73abd0ced33
Odralix <ossian.edstrom@live.se>
2019-04-11 11:34:22 +0200

    Added Working camera classes
    With projection matrix

--- Camera.cpp
+++ Camera.cpp
@@ -1,14 +1,92 @@
 #include "Camera.h"
 
-
-
-Camera::Camera()
+void Camera::reCalcCamVecs()
 {
+	{
+		glm::vec3 face;
+		face.x = cos(glm::radians(this->camYaw)) * cos(glm::radians(this->camPitch));
+		face.y = sin(glm::radians(this->camPitch));
+		face.z = sin(glm::radians(this->camYaw)) * cos(glm::radians(this->camPitch));
 
+		this->face = glm::normalize(face);
+
+		this->camRight = glm::normalize(glm::cross(this->face, this->worldUp));
+		this->camUp = glm::normalize(glm::cross(this->camRight, this->face));
+	}
 }
 
-
-Camera::~Camera()
+Camera::Camera(glm::vec3 pos, glm::vec3 up, float camYaw, float camPitch, float width, float height, float nearPlane, float farPlane)
+	: face(glm::vec3(0, 0, -1)), mSpeed(3.5f), sensitivity(0.15f)//, zoom(40.0f)
 {
+	this->camPos = pos;
+	this->worldUp = up;
+	this->camYaw = camYaw;
+	this->camPitch = camPitch;
 
+	this->width = width;
+	this->height = height;
+
+	this->nearPlane = nearPlane;
+	this->farPlane = farPlane;
+
+	setProjMat(this->width, this->height, this->nearPlane, this->farPlane);
+
+	reCalcCamVecs();
+}
+
+void Camera::FPSCamControls(GLFWwindow * window, float deltaTime)
+{
+	float camSpeed = this->mSpeed*deltaTime;
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		this->camPos -= camSpeed * this->camRight;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		this->camPos += camSpeed * this->camRight;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		this->camPos += camSpeed * this->face;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		this->camPos -= camSpeed * this->face;
+}
+
+glm::mat4 Camera::GetViewMatrix()
+{
+	//Calculates the view matrix given the camera positions.
+	return glm::lookAt(this->camPos, this->camPos + this->face, this->camUp);
+}
+
+glm::mat4 Camera::GetProjectionMatrix()
+{
+	return this->projMat;
+}
+
+void Camera::setWinSize(float width, float height)
+{
+	this->width = width;
+	this->height = height;
+
+	setProjMat(this->width, this->height, this->nearPlane, this->farPlane);
+}
+
+void Camera::pitchYawMouse(float xOffset, float yOffset, bool limitPitch = true)
+{
+	xOffset *= this->sensitivity;
+	yOffset *= this->sensitivity;
+
+	this->camYaw += xOffset;
+	this->camPitch += yOffset;
+
+	if (limitPitch)
+	{
+		if (this->camPitch > 89.0f)
+			this->camPitch = 89.0f;
+		if (this->camPitch < -89.0f)
+			this->camPitch = 89.0f;
+	}
+
+	reCalcCamVecs();
+}
+
+void Camera::setProjMat(float width, float height, float nearPlane, float farPlane)
+{
+	this->projMat = glm::perspective((3.14f*0.45f), width / height, nearPlane, farPlane);
 }
--- Camera.h
+++ Camera.h
@@ -1,8 +1,58 @@
 #pragma once
+
+#include "Headers.h"
+
+
+
 class Camera
 {
+private:
+
+	void reCalcCamVecs();
+
 public:
-	Camera();
-	~Camera();
+	glm::vec3 camPos;
+	glm::vec3 face;
+	glm::vec3 worldUp;
+	glm::vec3 camUp;
+	glm::vec3 camRight;
+
+	glm::mat4 projMat;
+
+	float camYaw;
+	float camPitch;
+
+	//Projection variables.
+	float width;
+	float height;
+	float nearPlane;
+	float farPlane;
+
+
+	//Should only apply to some cameras.
+	float mSpeed;
+	//float zoom;
+	float sensitivity;
+
+	//The Camera Constructor
+	Camera(glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 up = glm::vec3(0, 1, 0), float camYaw = -90.0f, float camPitch = 0.0f, float width = 640.0f, float height = 480.0f, float nearPlane = 0.1f, float farPlane = 200.0f);
+
+	//WASD camera movement controls. Need only be called in gameLoop if you want to move the specific camera.
+	//Add custom key bindings for devs? Could send in letters as strings and use those as keybinds.
+	void FPSCamControls(GLFWwindow * window, float deltaTime);
+
+	//Calculates and returns the view matrix.
+	glm::mat4 GetViewMatrix();
+
+	glm::mat4 GetProjectionMatrix();
+
+	void setWinSize(float width, float height);
+
+	//Pitch and yaw dependent on mouse movement if active. If limitPitch true standard FPS cam, No Flipping.
+	void pitchYawMouse(float xOffset, float yOffset, bool limitPitch);
+
+	void setProjMat(float width, float height, float nearPlane, float farPlane);
+
 };
+
 
