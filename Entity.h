57854504325abbfa6aa5183e667b5e07eb9de7e7
Jerry <jerryronnegren@hotmail.com>
2019-04-23 11:44:40 +0200

    Made so that the primitive will create its own hitbox.

--- Entity.h
+++ Entity.h
@@ -6,7 +6,10 @@ class Entity
 {
 private:
 	Primitive entityMesh;
+
+	glm::vec3 boundingBoxCenter;
 	glm::vec3 boundingBoxSize;
+
 
 	bool collision;
 
@@ -14,12 +17,13 @@ public:
 	Entity();
 	~Entity();
 
+	void InitBoundingBox();
+
 	bool CheckCollision(Entity collidingCube);
 
 	void setMaterialID(unsigned int materialID);
 	void setPosition(glm::vec3 newPos);
-
-	void setCustomBBox(glm::vec3);
+	void SetBoundingBox(glm::vec3 BBoxCenter, glm::vec3 BBoxHalfSize); // Should be private maybe
 
 	Primitive getMeshData() const;
 	glm::vec3 getPosition() const;
