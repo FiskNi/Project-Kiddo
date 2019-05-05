4515fd01ab574f2e1848dd55d6008f6766cb2e95
Odralix <ossian.edstrom@live.se>
2019-05-05 17:59:17 +0200

    BoxHolder multiple box update

--- boxHolder.h
+++ boxHolder.h
@@ -1,20 +1,29 @@
 #pragma once
 
 #include "StaticEntity.h"
+#include "Material.h"
 
 class boxHolder : public StaticEntity
 {
 private:
 	glm::vec3 origPos;
 	glm::vec3 origBBPos;
+	bool visible;
+
+	Mesh aMesh;
 	
 public:
 	boxHolder(unsigned int i);
-	boxHolder(vertex * vertArr, unsigned int nrOfVerticies);
+	boxHolder(vertex * vertArr, unsigned int nrOfVerticies, unsigned int holderMatID, unsigned int boxMatID);
 	~boxHolder();
 
 	void puntBox();
 	void boxReturn();
+
+	void setVisible(bool aBool);
+	bool getVisible() const { return this->visible; }
+	void SetBoxHolderPosition(glm::vec3 pos);
+	Mesh GetHolderMeshData();
 
 
 };
