67737925fe080f91e888840f7a6d8615c5479929
Odralix <ossian.edstrom@live.se>
2019-05-06 15:18:26 +0200

    Add matID to Entity constructors

--- Entity.h
+++ Entity.h
@@ -36,7 +36,7 @@ private:
 
 public:
 	Entity(unsigned int i);
-	Entity(vertex* vertArr, unsigned int nrOfVerticies);
+	Entity(vertex* vertArr, unsigned int nrOfVerticies, unsigned int matID);
 	~Entity();
 
 	void InitBoundingBox();
--- RigidEntity.h
+++ RigidEntity.h
@@ -22,7 +22,7 @@ private:
 
 public:
 	RigidEntity(unsigned int i);
-	RigidEntity(vertex * vertArr, unsigned int nrOfVerticies);
+	RigidEntity(vertex * vertArr, unsigned int nrOfVerticies, unsigned int matID);
 	~RigidEntity();
 
 	void AddVelocity(float x, float y, float z);
